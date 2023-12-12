9f030f6ce290377c28a2ab5623aa9234baadf2ff
Denny Bucklin <dennis.bucklin@gmail.com>
2023-12-11 23:05:39 -0600

    added table builder

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from keys import HOLIDAY_API_KEY
+# from keys import HOLIDAY_API_KEY
 import docx
 from datetime import datetime, timedelta
 
@@ -23,23 +23,25 @@ def build_dates(start, end, weekdays):
     }
     '''
     week_to_day = {
-        'Sunday': 'u',
+        'Sunday': 'S',
         'Monday': 'M',
         'Tuesday': 'T',
         'Wednesday': 'W',
-        'Thursday': 'h',
+        'Thursday': 'R',
         'Friday': 'F',
-        'Saturday': 'S',
+        'Saturday': 'A',
     }
     date = start
     day = timedelta(days=1)
     calendar = {
-        "dates": ["Date"],
-        "holidays": ["Holiday"],
+        "dates": [],
+        "topics": [],
+        "holidays": [],
     }
     while date <= end:
         if week_to_day[date.strftime('%A')] in weekdays:
             calendar['dates'].append(date.strftime('%b %d'))
+            calendar['topics'].append('')
             calendar['holidays'].append('')  # add logic for holidays
         date += day
 
@@ -54,9 +56,10 @@ def get_input():
 
     while not weekdays:
         weekdays = input("\nWhich weekdays? Type a one letter code for each"
-                         " day, with no spaces: [uMTWhFS]\n")
+                         " day, with no spaces: [SMTWRFA]\n")
         for char in weekdays:
-            if char not in "uMTWhFS":
+            if char not in "SMTWRFA":
+                print("Bad day codes")
                 weekdays = None
 
     while not start:
@@ -77,6 +80,27 @@ def get_input():
     return start, end, weekdays
 
 
+def build_table(table, calendar):
+    hdr_cells = table.rows[0].cells
+    hdr_cells[0].text = hdr_cells[4].text = "Date"
+    hdr_cells[1].text = hdr_cells[5].text = "Topics"
+    hdr_cells[2].text = hdr_cells[6].text = "Assignments"
+    if len(calendar['dates']) % 2 == 1:
+        calendar['dates'].append("")
+        calendar['topics'].append('')
+        calendar['holidays'].append('')
+    i = 0
+    j = len(calendar['dates']) // 2
+    while i < len(calendar['dates']) // 2:
+        row_cells = table.add_row().cells
+        row_cells[0].text = calendar['dates'][i]
+        row_cells[4].text = calendar['dates'][j]
+        row_cells[2].text = calendar['holidays'][i]
+        row_cells[6].text = calendar['holidays'][j]
+        i += 1
+        j += 1
+
+
 # ############
 # MAIN TREE ##
 # ############
@@ -85,4 +109,6 @@ if __name__ == "__main__":
     class_dates = build_dates(start, end, weekdays)  # populate date list
 
     document = docx.Document('demo.docx')
+    table = document.add_table(rows=1, cols=7)
+    build_table(table, class_dates)
     document.save("demo.docx")
